f6d3fd9b8a7c2c44450bc83fc337528b8aaa92ec
BarteleBoom <162456357+BarteleBoom@users.noreply.github.com>
2024-03-07 16:48:00 +0100

    aggiornamento 1

---
+++ src/components/CardMusica.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let Nomealbum;
+	export let Artista;
+	export let durata;
+	export let anno;
+	export let image;
+</script>
+
+<!--  -->
+<div class="card_musica">
+	<img alt="copertina del disco" src={image} />
+	<p class="card_musica_disco">{Nomealbum}</p>
+	<p>{Artista}</p>
+	<div class="card_musica_footer">
+		<p>{durata}</p>
+		<p>{anno}</p>
+	</div>
+</div>
+
+<!--  -->
+<style>
+	.card_musica {
+		background-color: rgba(113, 202, 127, 0.838);
+		width: 300px;
+		border-radius: 20px;
+		overflow: hidden;
+		padding: 20px;
+		color: rgb(1, 90, 90);
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	.card_musica_disco {
+		font-size: 27px;
+	}
+
+	.card_musica_footer {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,51 @@
-<script lang="ts">
-	import { get_collection } from '$cms';
+<script>
+	import Cardmusica from '$components/CardMusica.svelte';
+	//
+	let dischi = [
+		{
+			artista: 'Grondante',
+			Nomealbum: 'Hola',
+			durata: '2h',
+			anno: '2025',
+			image:
+				'https://assets.culturabologna.it/0e99cf30-73ea-4f91-a6b0-93162ed65392-vicko-mozara-m82uh-vamhg-unsplash.jpg/658e92357ddaf83a37526e47323880e7e339b4c0.jpg'
+		},
+		{
+			artista: 'Geopardo',
+			Nomealbum: 'Cespugli',
+			durata: '3:00',
+			anno: '2030',
+			image: 'https://secret-garden.it/wp-content/uploads/2018/09/Cespuglio-sempreverde.jpg'
+		},
+		{
+			artista: 'Dante',
+			Nomealbum: 'Angeli',
+			durata: '0:00',
+			anno: '1999',
+			image: 'https://romeartweek.com/images/event/CE-2970-0862-0.jpg'
+		}
+	];
 </script>
 
-Home page
+<!--  -->
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
+</svelte:head>
+<!--  -->
+{#each dischi as disco}
+	<Cardmusica
+		Nomealbum={disco.Nomealbum}
+		Artista={disco.artista}
+		durata={disco.durata}
+		anno={disco.anno}
+		image={disco.image}
+	/>
+{/each}
+
+<Cardmusica
+	Nomealbum="Calendula"
+	Artista="Grondante"
+	durata="2:06"
+	anno="2020"
+	image="https://www.lerboristeria.com/imgs/erbario/foto/calendula.jpg"
+/>
